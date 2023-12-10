5bde55dc27a09411c3a2fba115fe08e20e47afa4
Tangoal <tanguylprs@gmail.com>
2023-12-10 14:27:05 +0100

    update

---
+++ src/components/PopupTypeForm.js
@@ -0,0 +1,11 @@
+import { PopupButton } from '@typeform/embed-react'
+
+function PopupTypeForm() {
+  return (
+    <PopupButton id="<CyBZPM1U>" style={{ fontSize: 20 }} className="my-button">
+      click to open form in popup
+    </PopupButton>
+  )
+}
+
+export default PopupTypeForm
